ecd502fe3c95ff0bb96b7ef58e2403e6bd36cd57
Leandro Segovia <ldlsegovia@gmail.com>
2014-07-05 22:05:05 -0300

    feat(chore): install active admin with fixed jquery-ui-rails dependency

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 HipchatExtensions::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
