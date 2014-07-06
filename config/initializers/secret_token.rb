3b5eb3ce8ebe7d33e79ecc765a619e6982b818af
Leandro Segovia <ldlsegovia@gmail.com>
2014-07-05 21:15:52 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HipchatExtensions::Application.config.secret_key_base = '59de6ee5826e2055c81f3eca41d109267eb44f12ab5581bd8c17cb702c63bf399270a89a3413a5790d4a7d25b02f425edd1922bfee7357bb6eedeb993ed0a746'
